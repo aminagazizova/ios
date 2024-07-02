79f9bd8972cccc0528555a55a02e659c63c0fdf0
Amina <144144307+aminagazizova@users.noreply.github.com>
2024-07-02 18:55:20 +0300

    домашняя работа 2

---
+++ HW2/HW2/HW2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HW2
+//
+//  Created by Амина Газизова on 02.07.2024.
+//
+//
+//import UIKit
+//
+//class SecondViewController: UIViewController {
+//    @IBOutlet weak var password_second: UITextField!
+//    @IBOutlet weak var login_second: UITextField!
+//    
+//    @IBOutlet weak var year: UIDatePicker!
+//    
+//    var login = ""
+//    var password = ""
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        login_second.text = "Email: \(login)"
+//        password_second.text = "Password: \(password)"
+//    }
+//
+//}
